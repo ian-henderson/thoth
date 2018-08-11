66032d43cf0b1c9ff6455af55ebb4fe0d2ac34e7
Ian Henderson <iancurtish@gmail.com>
2018-08-11 17:11:07 -0500

    formatted server.js

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 import { ApolloServer } from 'apollo-server-hapi';
-import Hapi from 'hapi';
+import { Server as HapiServer } from 'hapi';
 
 import dataSources from './src/dataSources';
 import { resolvers, typeDefs } from './src/graph';
 
-const apolloServer = new ApolloServer({ dataSources, resolvers, typeDefs });
-
-const hapiServer = new Hapi.server({ port: 4000 });
 
 const startServer = async () => {
-  try {
-    await apolloServer.applyMiddleware({ app: hapiServer });
-    await apolloServer.installSubscriptionHandlers(hapiServer.listener);
-    await hapiServer.start();
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+  const apolloServer = new ApolloServer({ dataSources, resolvers, typeDefs });
+  const hapiServer = new HapiServer({ port: 4000 });
+  await apolloServer.applyMiddleware({ app: hapiServer });
+  await apolloServer.installSubscriptionHandlers(hapiServer.listener);
+  await hapiServer.start();
 };
 
-startServer();
+startServer().catch(() => process.exit(1));
